c26ed306b93a7a519d0fbd82a6aa2f177de7ec83
PaulTrofimovFHSWF <trofimov.paul@fh-swf.de>
2023-04-13 23:20:30 +0200

    Tests hinzugefügt

--- Uebung1/src/WarmUp.java
+++ Uebung1/src/WarmUp.java
@@ -76,4 +76,5 @@ public class WarmUp {
         }
         return sum;
     }
+
 }
---
+++ Uebung1/src/WarmUpTest.java
@@ -0,0 +1,14 @@
+package Uebung1.src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarmUpTest {
+
+    @org.junit.jupiter.api.Test
+    void ermittlePrimzahl() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void berechneQuersumme() {
+    }
+}
